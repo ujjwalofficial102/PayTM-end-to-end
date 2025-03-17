e3547cdf73dcb23eca6163d577493895aacb76d4
ujjwalofficial102 <ujjwalmishra102@gmail.com>
2025-03-17 19:50:40 +0530

    routes file created, jwt and cors implemented

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,15 @@
 const express = require("express");
+const mainRouter = require("./routes/index");
+const cors = require("cors");
+const { JWT_SECRET } = require("./config");
 
+const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1", mainRouter);
+
+app.listen(3001, () => {
+  console.log("server is running at port 3001");
+});
