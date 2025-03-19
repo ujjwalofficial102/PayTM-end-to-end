da533bbcaab47d9f3688e50fa78a2c2ca9a80787
ujjwalofficial102 <ujjwalmishra102@gmail.com>
2025-03-19 21:37:08 +0530

    bank schema added

--- backend/db.js
+++ backend/db.js
@@ -5,13 +5,26 @@ mongoose.connect(
 );
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  firstName: String,
-  lastName: String,
-  password: String,
+  username: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  password: { type: String, required: true },
+});
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
 });
 
-const User = mongoose.model("users", userSchema);
+const User = mongoose.model("User", userSchema);
+const Account = mongoose.model("Account", accountSchema);
 module.exports = {
   User,
+  Account,
 };
